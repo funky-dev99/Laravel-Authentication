db051a616e6382ee483c2240df8b04c8ddfcfe80
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-05-23 17:51:11 +0530

    Reg Page

---
+++ laravel_auth/lib/authentication/reg_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RegPage extends StatefulWidget {
+  const RegPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegPage> createState() => _RegPageState();
+}
+
+class _RegPageState extends State<RegPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      
+    );
+  }
+}
